a9998c677e28d461bacc4130269c18c9b8854650
eankeen <24364012+eankeen@users.noreply.github.com>
2020-04-17 22:18:12 -0700

    feat(ordinal): create ansible playbook basic provisioning

---
+++ ordinal/createLinks.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -eu
+
+fileWithOrdinalContainerNames="../foundation/ansible-remote/roles/lxd/vars/main.yml"
+json="$(python -c "import json,yaml; print(json.dumps(yaml.safe_load(open(\"$fileWithOrdinalContainerNames\", \"r\").read())))")"
+
+readarray -t containerNames < <(echo "$json" | jq -c '.containers[].name' | sed s/\\\"//g)
+for containerName in "${containerNames[@]}"
+do
+    keyFileName="$containerName.key"
+    echo "creating link to original $keyFileName file"
+
+    ln -sf "../../foundation/$keyFileName" .
+done
